cb3878ac7325ba4704368294a70248952f17a89b
IkerCG2003 <100007043.joan23@fje.edu>
2023-10-31 15:53:40 +0100

    View -> Login / Register

---
+++ view/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Log in / Sign in</title>
+    </head>
+
+    <body>
+        <form action="" method="post">
+            <label for="user">Usuario:</label>
+            <input type="text" name="user" id="user" placeholder="Introduce el usuario">
+            <label for="contra">Contraseña:</label>
+            <input type="password" name="contra" id="contra" placeholder="Introduce la contraseña">
+        </form>
+    </body>
+</html>
