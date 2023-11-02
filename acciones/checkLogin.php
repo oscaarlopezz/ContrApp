2e872372879b7348b73dd4213a5815ad9723cbf3
IkerCG2003 <100007043.joan23@fje.edu>
2023-11-02 16:13:49 +0100

    Corrección login - 02.11.2023

--- acciones/checkLogin.php
+++ acciones/checkLogin.php
@@ -2,16 +2,17 @@
     session_start(); // Iniciamos sesión.
 
     $user = $_SESSION['user']; // Recuperamos el valor de la sesión user.
+    $passEncriptada = hash("sha256", $_SESSION['pass']);
 
     include_once('../herramientas/conexion.php'); // Incluimos el fichero de conexión.
 
-    $consultaSeleccionarUser = "SELECT * FROM usuarios WHERE username = ?"; // Seleccionamos el contenido de la tabla user.
+    $consultaSeleccionarUser = "SELECT * FROM usuarios WHERE username = ? AND contraseña = ?"; // Seleccionamos el contenido de la tabla user.
 
     $stmtSeleccionarUser = mysqli_stmt_init($conn); 
 
     if (mysqli_stmt_prepare($stmtSeleccionarUser, $consultaSeleccionarUser)) 
     {
-        mysqli_stmt_bind_param($stmtSeleccionarUser, "s", $user);
+        mysqli_stmt_bind_param($stmtSeleccionarUser, "ss", $user, $passEncriptada);
 
         if (mysqli_stmt_execute($stmtSeleccionarUser)) 
         {
@@ -19,22 +20,17 @@
 
             if ($row = mysqli_fetch_assoc($result)) 
             {   
-                $passEncriptada = hash("sha256", $_SESSION['pass']);
-                $passBD = $row['contraseña']; // Accede a la contraseña desde el arreglo asociativo $row
-            
-                if ($passBD === $passEncriptada) // Compara ambas contraseñas
-                {
-                    echo "¡Bienvenido " . $_SESSION['user'] . "! La contraseña es CORRECTA"; // Si las contraseñas coinciden te da la bienvenida
-                } 
-                
-                else
-                {
-                    header('Location ../view/login.php?Usuario o contraseñas INCORRECTA.'); // Se muestra el mensaje de error.
-                }
+                echo "¡Bienvenido " . $_SESSION['user'] . "! La contraseña es CORRECTA"; // Si las contraseñas coinciden te da la bienvenida
             }
+                
+            else
+            {
+                header('Location ../view/login.php?Usuario o contraseña incorrectos'); // Se muestra el mensaje de error.
+            }
+        }
                         
-        } 
     } 
+  
     
     else 
     {
