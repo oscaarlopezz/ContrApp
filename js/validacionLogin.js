7ec351df67b78d8f7ff335698b5491c349bc48c5
IkerCG2003 <100007043.joan23@fje.edu>
2023-11-02 18:35:35 +0100

    Cambios en los ficheros .js - 02.11.2023

--- js/validacionLogin.js
+++ js/validacionLogin.js
@@ -1,49 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () 
 {
-    // Obtiene referencias a los campos de usuario y contraseña
-    const userF = document.getElementById('user');
-    const passF = document.getElementById('pass');
+    // Obtiene referencias a los campos de usuario, email, contraseña y el botón de envío
+    const userField = document.getElementById('user');
+    const passField = document.getElementById('pass');
+    const btnEnviar = document.getElementById('btnEnviar');
 
-    // Agrega un evento de escucha para el campo de usuario
-    userF.addEventListener('input', function () 
+    // Expresión regular para validar el usuario (solo letras mayúsculas y minúsculas)
+    const userRegex = /^[a-zA-Z]*$/;
+
+    // Agrega eventos de escucha para los campos de usuario, email y contraseña
+    userField.addEventListener('input', validateForm);
+    passField.addEventListener('input', validateForm);
+
+    // Define la función "validateForm" que se ejecutará cuando cambie el contenido de los campos
+    function validateForm() 
     {
-        // Obtiene el valor del campo de usuario y la alerta de usuario
-        const user = document.getElementById('user');
-        const alertauser = document.getElementById('alertauser');
+        const userValue = userField.value.trim();
+        const passValue = passField.value.trim();
 
-        // Comprueba si el valor del campo de usuario cumple con la validación de la función "validarUser"
-        if (validarUser(user.value)) 
+        // Valida el campo de usuario
+        const alertaUser = document.getElementById('alertauser');
+        if (userValue.length < 1 || !userRegex.test(userValue)) 
         {
-            alertauser.style.display = 'none'; // Oculta la alerta de usuario si es válido
-        } 
-        
-        else if (user.value.length < 1) {
-
-            alertauser.style.display = 'none'; // Oculta la alerta de usuario si el campo está vacío
-        } 
-        
-        else
-        {
-            alertauser.style.display = 'block'; // Muestra la alerta de usuario si no cumple con la validación
-        }
-    });
-
-    // Agrega un evento de escucha para el campo de contraseña
-    passF.addEventListener('input', function () 
-    {
-        // Obtiene el valor del campo de contraseña y la alerta de contraseña
-        const pass = document.getElementById('pass');
-        const alertapass = document.getElementById('alertapass');
-
-        // Comprueba la longitud de la contraseña y muestra la alerta correspondiente
-        if (pass.value.length !== 9) 
-        {
-            alertapass.style.display = 'block'; // Muestra la alerta de contraseña si no tiene 9 caracteres
+            alertaUser.style.display = 'block'; // Muestra la alerta de usuario si no cumple con la validación
         } 
         
         else 
         {
-            alertapass.style.display = 'none'; // Oculta la alerta de contraseña en otros casos
+            alertaUser.style.display = 'none'; // Oculta la alerta de usuario si es válido
         }
-    });
+
+        // Valida el campo de contraseña
+        const alertaPass = document.getElementById('alertapass');
+        if (passValue.length !== 9) 
+        {
+            alertaPass.style.display = 'block'; // Muestra la alerta de contraseña si no tiene exactamente 9 caracteres
+        } 
+        
+        else 
+        {
+            alertaPass.style.display = 'none'; // Oculta la alerta de contraseña en otros casos
+        }
+
+        // Comprueba si todos los campos son válidos y habilita/deshabilita el botón de envío
+        if (userRegex.test(userValue) && passValue.length === 9) 
+        {
+            btnEnviar.removeAttribute("disabled"); // Habilita el botón de envío si todos los campos son válidos
+        } 
+        
+        else 
+        {
+            btnEnviar.setAttribute("disabled", "disabled"); // Deshabilita el botón de envío si al menos uno de los campos no es válido
+        }
+    }
 });
--- js/validacionRegister.js
+++ js/validacionRegister.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () 
+{
     // Obtiene referencias a los campos de usuario, email, contraseña y el botón de envío
     const userField = document.getElementById('user');
     const emailField = document.getElementById('email');
@@ -17,39 +18,56 @@ document.addEventListener("DOMContentLoaded", function () {
     passField.addEventListener('input', validateForm);
 
     // Define la función "validateForm" que se ejecutará cuando cambie el contenido de los campos
-    function validateForm() {
+    function validateForm() 
+    {
         const userValue = userField.value.trim();
         const emailValue = emailField.value.trim();
         const passValue = passField.value.trim();
 
         // Valida el campo de usuario
         const alertaUser = document.getElementById('alertauser');
-        if (userValue.length < 1 || !userRegex.test(userValue)) {
+        if (userValue.length < 1 || !userRegex.test(userValue)) 
+        {
             alertaUser.style.display = 'block'; // Muestra la alerta de usuario si no cumple con la validación
-        } else {
+        } 
+        
+        else 
+        {
             alertaUser.style.display = 'none'; // Oculta la alerta de usuario si es válido
         }
 
         // Valida el campo de email
         const alertaEmail = document.getElementById('alertaEmail');
-        if (emailValue.length < 1 || !emailRegex.test(emailValue)) {
+        if (emailValue.length < 1 || !emailRegex.test(emailValue))
+        {
             alertaEmail.style.display = 'block'; // Muestra la alerta de email si no cumple con la validación
-        } else {
+        } 
+        
+        else 
+        {
             alertaEmail.style.display = 'none'; // Oculta la alerta de email si es válido
         }
 
         // Valida el campo de contraseña
         const alertaPass = document.getElementById('alertapass');
-        if (passValue.length !== 9) {
+        if (passValue.length !== 9) 
+        {
             alertaPass.style.display = 'block'; // Muestra la alerta de contraseña si no tiene exactamente 9 caracteres
-        } else {
+        } 
+        
+        else 
+        {
             alertaPass.style.display = 'none'; // Oculta la alerta de contraseña en otros casos
         }
 
         // Comprueba si todos los campos son válidos y habilita/deshabilita el botón de envío
-        if (userRegex.test(userValue) && emailRegex.test(emailValue) && passValue.length === 9) {
+        if (userRegex.test(userValue) && emailRegex.test(emailValue) && passValue.length === 9) 
+        {
             btnEnviar.removeAttribute("disabled"); // Habilita el botón de envío si todos los campos son válidos
-        } else {
+        } 
+        
+        else 
+        {
             btnEnviar.setAttribute("disabled", "disabled"); // Deshabilita el botón de envío si al menos uno de los campos no es válido
         }
     }
